ba255f2a3b0ecb52041dabdfcac8253db5dc2dfc
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-02-01 08:48:05 +0100

    feat(app): Adds first angular app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,11 @@
-/**
- * Created by hirsch on 31/01/16.
- */
-
 import '../assets/scss/main.scss';
 
+import 'angular';
+
 console.info('Created by hirsch on 31gi/01/16.');
+
+angular
+  .module('app', [])
+  .run(() => {
+    console.info('Angular is ready!');
+  });
