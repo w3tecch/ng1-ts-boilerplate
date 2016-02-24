c356edae11dbac129203c965f2cf5851c0d9c2ec
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-02-24 07:37:29 +0100

    added mediator

--- src/app/modules/home/home.controller.ts
+++ src/app/modules/home/home.controller.ts
@@ -1,4 +1,5 @@
 import Logger from './../../services/utils/logger.service.ts';
+import {mediatorService} from './../../services/utils/mediator.service.ts';
 
 export default class HomeController {
   public static $inject = [];
@@ -9,8 +10,15 @@ export default class HomeController {
   constructor() {
     this._logger = new Logger('app.services.HomeController');
     this._logger.info('Test');
-    //this._logger = new Logger('app.services.HomeController');
-    //this._logger.info(' constructor', Logger.DEBUG);
+
+    let testChannelSubscriber = mediatorService.subscribe('test:channel');
+    testChannelSubscriber((a) => {
+      this._logger.info('test:channel', a);
+    });
+
+    let testChannelPublisher = mediatorService.publish('test:channel');
+    testChannelPublisher('gugus 1');
+    testChannelPublisher('gugus 2');
   }
 
   public sayHello(): string {
