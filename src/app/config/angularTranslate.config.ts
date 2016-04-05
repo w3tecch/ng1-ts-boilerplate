7d5dbb87f7f36a689ab1e34ebcfad93785a51751
David Weber <david.weber@w3tec.ch>
2016-04-05 22:39:57 +0200

    add sanitize strategy

--- src/app/config/angularTranslate.config.ts
+++ src/app/config/angularTranslate.config.ts
@@ -5,6 +5,7 @@ const angularTranslate = ($translateProvider) => {
   $translateProvider.translations('en', require('./../../assets/i18n/en.json'));
   $translateProvider.translations('de', require('./../../assets/i18n/de.json'));
   $translateProvider.preferredLanguage('en');
+  $translateProvider.useSanitizeValueStrategy('escapeParameters');
 };
 
 angularTranslate.$inject = ['$translateProvider'];
